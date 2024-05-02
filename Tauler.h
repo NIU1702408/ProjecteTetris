5f919dd39b86f22daabcb0fc91603054b7597426
NIU1702408 <149057792+NIU1702408@users.noreply.github.com>
2024-05-02 17:04:30 +0200

    Update Tauler.h

--- Tauler.h
+++ Tauler.h
@@ -1,22 +1,26 @@
 #ifndef TAULER_H
 #define TAULER_H
-#include "figura.h"
+#include "Figura.h"
+
+const int MAX_FILA = 8;
+const int MAX_COL = 8;
 
 class Tauler
 {
 public:
 	Tauler();
-	Tauler(ColorFigura tauler[MAX_FILA_TAULER][MAX_FILA_TAULER]);
+	Tauler(ColorFigura tauler[MAX_COL][MAX_FILA]);
 
 	ColorFigura getTauler(const int& x, const int& y) const { return m_tauler[x][y]; }
 	void setTauler(ColorFigura color, int posX, int posY) { m_tauler[posX][posY] = color; }
 
 	bool esMovimentValid(const Figura& figura,const Posicio& pos) const;
-	void eliminarFilesCompletades();
+	int eliminarFilesCompletades();
+	void eliminarFigura(Figura figura, const Posicio& pos);
 	void collocarFigura(Figura figura,const Posicio& pos);
 private:
 	bool posicioValida(const Posicio& pos) const;
 
-	ColorFigura m_tauler[MAX_FILA_TAULER][MAX_FILA_TAULER];
+	ColorFigura m_tauler[MAX_COL][MAX_FILA];
 };
 #endif
