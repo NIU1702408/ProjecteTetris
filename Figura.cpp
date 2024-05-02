048b690f5dcbc7073090df5f177671a75e6d0d7d
NIU1709419 <149013299+NIU1709419@users.noreply.github.com>
2024-05-02 21:52:54 +0200

    Update Figura.cpp

--- Figura.cpp
+++ Figura.cpp
@@ -2,115 +2,205 @@
 
 Figura::Figura()
 {
+	m_mida = 0;
 	m_color = NO_COLOR;
 	m_tipusFigura = NO_FIGURA;
+	m_posicio.vertical = 0;
+	m_posicio.horitzontal = 0;
 
 	for (int i = 0; i < MAX_ALCADA; i++)
 		for (int j = 0; j < MAX_AMPLADA; j++)
-			m_matriuFigura[i][j] = false;
+			m_figura[i][j] = NO_COLOR;
 
 }
 
-Figura::Figura(TipusFigura tipusFigura, ColorFigura color)
+Figura::Figura(TipusFigura tipusFigura)
 {
+	m_mida = 0;
 	m_tipusFigura = tipusFigura;
-	m_color = color;
-	int mida = 0;
-
-	switch (tipusFigura)
-	{
-		case FIGURA_T:
-		case FIGURA_L:
-		case FIGURA_J:
-		case FIGURA_Z:
-		case FIGURA_S:
-			mida = 3;
-			break;
-		case FIGURA_O:
-			mida = 2;
-			break;
-		case FIGURA_I:
-			mida = 4;
-			break;
-	}
-
-	m_mida = mida;
+	m_posicio.vertical = 0;
+	m_posicio.horitzontal = 0;
 
 	for (int i = 0; i < MAX_ALCADA; i++)
 		for (int j = 0; j < MAX_AMPLADA; j++)
-			m_matriuFigura[i][j] = false;
+			m_figura[i][j] = NO_COLOR;
 
-	void inicialitzaFigura();
-}
-
-void Figura::inicialitzaFigura()
-{
 	switch (m_tipusFigura)
 	{
-		case FIGURA_O:
-			m_matriuFigura[0][0] = true;
-			m_matriuFigura[0][1] = true;
-			m_matriuFigura[1][0] = true;
-			m_matriuFigura[1][1] = true;
-			break;
-		case FIGURA_I:
-			m_matriuFigura[1][0] = true;
-			m_matriuFigura[1][1] = true;
-			m_matriuFigura[1][2] = true;
-			m_matriuFigura[1][3] = true;
-			break;
-		case FIGURA_T:
-			m_matriuFigura[0][1] = true;
-			m_matriuFigura[1][0] = true;
-			m_matriuFigura[1][1] = true;
-			m_matriuFigura[1][2] = true;
-			break;
-		case FIGURA_L:
-			m_matriuFigura[0][1] = true;
-			m_matriuFigura[1][1] = true;
-			m_matriuFigura[2][1] = true;
-			m_matriuFigura[2][2] = true;
-			break;
-		case FIGURA_J:
-			m_matriuFigura[0][1] = true;
-			m_matriuFigura[1][1] = true;
-			m_matriuFigura[2][0] = true;
-			m_matriuFigura[2][1] = true;
-			break;
-		case FIGURA_Z:
-			m_matriuFigura[0][0] = true;
-			m_matriuFigura[0][1] = true;
-			m_matriuFigura[1][1] = true;
-			m_matriuFigura[1][2] = true;
-			break;
-		case FIGURA_S:
-			m_matriuFigura[0][1] = true;
-			m_matriuFigura[0][2] = true;
-			m_matriuFigura[1][0] = true;
-			m_matriuFigura[1][1] = true;
-			break;
+	case FIGURA_O:
+		m_mida = 2;
+		m_color = COLOR_GROC;
+		m_figura[0][0] = COLOR_GROC;
+		m_figura[0][1] = COLOR_GROC;
+		m_figura[1][0] = COLOR_GROC;
+		m_figura[1][1] = COLOR_GROC;
+		break;
+	case FIGURA_I:
+		m_mida = 4;
+		m_color = COLOR_BLAUCEL;
+		m_figura[1][0] = COLOR_BLAUCEL;
+		m_figura[1][1] = COLOR_BLAUCEL;
+		m_figura[1][2] = COLOR_BLAUCEL;
+		m_figura[1][3] = COLOR_BLAUCEL;
+		break;
+	case FIGURA_T:
+		m_mida = 3;
+		m_color = COLOR_MAGENTA;
+		m_figura[0][1] = COLOR_MAGENTA;
+		m_figura[1][0] = COLOR_MAGENTA;
+		m_figura[1][1] = COLOR_MAGENTA;
+		m_figura[1][2] = COLOR_MAGENTA;
+		break;
+	case FIGURA_L:
+		m_mida = 3;
+		m_color = COLOR_TARONJA;
+		m_figura[0][1] = COLOR_TARONJA;
+		m_figura[1][1] = COLOR_TARONJA;
+		m_figura[2][1] = COLOR_TARONJA;
+		m_figura[2][2] = COLOR_TARONJA;
+		break;
+	case FIGURA_J:
+		m_mida = 3;
+		m_color = COLOR_BLAUFOSC;
+		m_figura[0][1] = COLOR_BLAUFOSC;
+		m_figura[1][1] = COLOR_BLAUFOSC;
+		m_figura[2][0] = COLOR_BLAUFOSC;
+		m_figura[2][1] = COLOR_BLAUFOSC;
+		break;
+	case FIGURA_Z:
+		m_mida = 3;
+		m_color = COLOR_VERMELL;
+		m_figura[0][0] = COLOR_VERMELL;
+		m_figura[0][1] = COLOR_VERMELL;
+		m_figura[1][1] = COLOR_VERMELL;
+		m_figura[1][2] = COLOR_VERMELL;
+		break;
+	case FIGURA_S:
+		m_mida = 3;
+		m_color = COLOR_VERD;
+		m_figura[0][1] = COLOR_VERD;
+		m_figura[0][2] = COLOR_VERD;
+		m_figura[1][0] = COLOR_VERD;
+		m_figura[1][1] = COLOR_VERD;
+		break;
 	}
 }
 
-void Figura::transposarFigura()
+void Figura::incialitza (TipusFigura tipusFigura)
 {
-	bool transposada[MAX_ALCADA][MAX_AMPLADA];
+	m_mida = 0;
+	m_tipusFigura = tipusFigura;
+	m_posicio.vertical = 0;
+	m_posicio.horitzontal = 0;
 
-	for (int i = 0; i < MAX_ALCADA; ++i) {
-		for (int j = 0; j < MAX_AMPLADA; ++j) {
-			transposada[j][i] = m_matriuFigura[i][j];
-		}
+	for (int i = 0; i < MAX_ALCADA; i++)
+		for (int j = 0; j < MAX_AMPLADA; j++)
+			m_figura[i][j] = NO_COLOR;
+
+	switch (m_tipusFigura)
+	{
+	case FIGURA_O:
+		m_mida = 2;
+		m_color = COLOR_GROC;
+		m_figura[0][0] = COLOR_GROC;
+		m_figura[0][1] = COLOR_GROC;
+		m_figura[1][0] = COLOR_GROC;
+		m_figura[1][1] = COLOR_GROC;
+		break;
+	case FIGURA_I:
+		m_mida = 4;
+		m_color = COLOR_BLAUCEL;
+		m_figura[1][0] = COLOR_BLAUCEL;
+		m_figura[1][1] = COLOR_BLAUCEL;
+		m_figura[1][2] = COLOR_BLAUCEL;
+		m_figura[1][3] = COLOR_BLAUCEL;
+		break;
+	case FIGURA_T:
+		m_mida = 3;
+		m_color = COLOR_MAGENTA;
+		m_figura[0][1] = COLOR_MAGENTA;
+		m_figura[1][0] = COLOR_MAGENTA;
+		m_figura[1][1] = COLOR_MAGENTA;
+		m_figura[1][2] = COLOR_MAGENTA;
+		break;
+	case FIGURA_L:
+		m_mida = 3;
+		m_color = COLOR_TARONJA;
+		m_figura[0][2] = COLOR_TARONJA;
+		m_figura[1][0] = COLOR_TARONJA;
+		m_figura[1][1] = COLOR_TARONJA;
+		m_figura[1][2] = COLOR_TARONJA;
+		break;
+	case FIGURA_J:
+		m_mida = 3;
+		m_color = COLOR_BLAUFOSC;
+		m_figura[0][0] = COLOR_BLAUFOSC;
+		m_figura[1][0] = COLOR_BLAUFOSC;
+		m_figura[1][1] = COLOR_BLAUFOSC;
+		m_figura[1][2] = COLOR_BLAUFOSC;
+		break;
+	case FIGURA_Z:
+		m_mida = 3;
+		m_color = COLOR_VERMELL;
+		m_figura[0][0] = COLOR_VERMELL;
+		m_figura[0][1] = COLOR_VERMELL;
+		m_figura[1][1] = COLOR_VERMELL;
+		m_figura[1][2] = COLOR_VERMELL;
+		break;
+	case FIGURA_S:
+		m_mida = 3;
+		m_color = COLOR_VERD;
+		m_figura[0][1] = COLOR_VERD;
+		m_figura[0][2] = COLOR_VERD;
+		m_figura[1][0] = COLOR_VERD;
+		m_figura[1][1] = COLOR_VERD;
+		break;
 	}
-
-	for (int i = 0; i < MAX_ALCADA; ++i) {
-		for (int j = 0; j < MAX_AMPLADA; ++j) {
-			m_matriuFigura[i][j] = transposada[i][j];
-		}
-	}
-
 }
 
-void Figura::invertirFigura()
+void Figura::girar(const DireccioGir& gir)
 {
+	ColorFigura transposada[MAX_ALCADA][MAX_AMPLADA];
+	ColorFigura invertir[MAX_ALCADA][MAX_AMPLADA];
 
+	// Transposa matriu
+	for (int i = 0; i < m_mida; ++i)
+		for (int j = 0; j < m_mida; ++j)
+			transposada[j][i] = m_figura[i][j];
+
+	for (int i = 0; i < m_mida; ++i)
+		for (int j = 0; j < m_mida; ++j)
+			m_figura[i][j] = transposada[i][j];
+
+	//Inverteix matriu
+	if (gir == GIR_HORARI)
+	{
+		for (int i = 0; i < m_mida; ++i)
+			for (int j = 0; j < m_mida; ++j)
+				invertir[i][j] = m_figura[i][m_mida - 1 - j];
+	}
+	else
+		for (int i = 0; i < m_mida; ++i)
+			for (int j = 0; j < m_mida; ++j)
+				invertir[i][j] = m_figura[m_mida - 1 - i][j];
+
+	for (int i = 0; i < m_mida; ++i)
+		for (int j = 0; j < m_mida; ++j)
+			m_figura[i][j] = invertir[i][j];
+}
+
+void Figura::baixar(const int& dirY)
+{
+	if (dirY == -1)
+		m_posicio.vertical--;
+	else
+		m_posicio.vertical++;
+}
+
+void Figura::desplacar(const int& dirX)
+{
+	if (dirX == -1)
+		m_posicio.horitzontal--;
+	else
+		m_posicio.horitzontal++;
 }
