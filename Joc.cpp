cdeb11baffdf07751963b09d1999430894745a64
NIU1709419 <149013299+NIU1709419@users.noreply.github.com>
2024-05-02 21:53:49 +0200

    Update Joc.cpp

--- Joc.cpp
+++ Joc.cpp
@@ -2,29 +2,118 @@
 
 void Joc::inicialitza(const string& nomFitxer)
 {
+	Posicio posicio;
+	int tipus, gir;
+	ifstream fitxer;
+	m_figuraCollocada = false;
 
+	fitxer.open(nomFitxer);
+
+	if (fitxer.is_open())
+	{
+		fitxer >> tipus;
+		m_figura.incialitza(TipusFigura(tipus));
+
+		fitxer >> posicio.vertical >> posicio.horitzontal;
+
+		posicio.vertical--;
+		posicio.horitzontal--;
+
+		m_figura.setPosicio(posicio);
+		m_posicio = posicio;
+		
+		fitxer >> gir;
+		for (int i = 0; i < gir; i++)
+			m_figura.girar(GIR_HORARI);
+
+		for (int i = 0; i < MAX_COL; i++)
+		{
+			for (int j = 0; j < MAX_FILA; j++)
+			{
+				int color;
+				fitxer >> color;
+
+				m_tauler.setTauler(ColorFigura(color), i, j);
+			}
+		}
+
+		fitxer.close();
+	}
 }
 
 bool Joc::giraFigura(DireccioGir direccio)
 {
+	bool gira = true;
+	m_figura.girar(direccio);
 
+	if (!m_tauler.esMovimentValid(m_figura, m_posicio))
+	{
+		if (direccio == GIR_HORARI)
+			m_figura.girar(GIR_ANTI_HORARI);
+		else
+			m_figura.girar(GIR_HORARI);
+		gira = false;
+	}
+
+	return gira;
 }
 
 bool Joc::mouFigura(int dirX)
 {
-	bool mou;
+	bool mou = true;
+	m_figura.desplacar(dirX);
 
+	if (!m_tauler.esMovimentValid(m_figura, m_figura.getPosicio()))
+	{
+		if (dirX == -1)
+			m_figura.desplacar(1);
+		else
+			m_figura.desplacar(-1);
+		mou = false;
+	}
+	else
+		m_posicio = m_figura.getPosicio();
 
 	return mou;
 }
 
 int Joc::baixaFigura()
 {
+	int nFiles = 0;
+	m_figura.baixar(1);
 
-	return 0;
+	if (!m_tauler.esMovimentValid(m_figura, m_figura.getPosicio()))
+	{
+		m_figura.baixar(-1);
+		m_tauler.collocarFigura(m_figura, m_figura.getPosicio());
+		m_figuraCollocada = true;
+		nFiles = m_tauler.eliminarFilesCompletades();
+	}
+	else
+		m_posicio = m_figura.getPosicio();
+
+	return nFiles;
 }
 
 void Joc::escriuTauler(const string& nomFitxer)
 {
-	
+	int i, j;
+	ofstream fitxer;
+
+	fitxer.open(nomFitxer);
+
+	if (fitxer.is_open())
+	{
+		if (!m_figuraCollocada)
+			m_tauler.collocarFigura(m_figura, m_posicio);
+		for (int i = 0; i < MAX_COL; i++)
+		{
+			for (int j = 0; j < MAX_FILA; j++)
+				fitxer << int(m_tauler.getTauler(i, j)) << " ";
+			fitxer << endl;
+		}
+		if(!m_figuraCollocada)
+			m_tauler.eliminarFigura(m_figura, m_posicio);
+		fitxer.close();
+	}
 }
