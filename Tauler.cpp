032db3389ad6de467f09bba4003bffa6e461d4fd
NIU1709419 <149013299+NIU1709419@users.noreply.github.com>
2024-05-02 21:54:40 +0200

    Update Tauler.cpp

--- Tauler.cpp
+++ Tauler.cpp
@@ -2,65 +2,110 @@
 
 Tauler::Tauler()
 {
-	for(int i = 0; i < MAX_FILA; i++)
+	for (int i = 0; i < MAX_COL; i++)
 		for (int j = 0; j < MAX_FILA; j++)
-			m_tauler[i][j] = NO_COLOR;
+			m_tauler[i][j] = COLOR_NEGRE;
 }
 
-Tauler::Tauler(ColorFigura tauler[MAX_FILA][MAX_FILA])
+Tauler::Tauler(ColorFigura tauler[MAX_COL][MAX_FILA])
 {
-	for (int i = 0; i < MAX_FILA; i++)
+	for (int i = 0; i < MAX_COL; i++)
 		for (int j = 0; j < MAX_FILA; j++)
 			m_tauler[i][j] = tauler[i][j];
 }
 
-bool Tauler::esMovimentValid(Figura figura, int fila, int columna)
+bool Tauler::posicioValida(const Posicio& pos) const 
 {
-    bool esValid = true;
+	bool esValid = true;
 
-    if (fila < 0 || columna < 0 || fila + figura.getMida() > MAX_FILA || columna + figura.getMida() > MAX_COL) {
-        esValid = false;
-    }
+	if (0 > pos.horitzontal || pos.horitzontal >= MAX_FILA || pos.vertical >= MAX_COL )
+		esValid = false;
+	else
+	{
+		if (m_tauler[pos.vertical][pos.horitzontal] != COLOR_NEGRE)
+			esValid = false;
+	}
 
-    // Comprobar si la figura colisiona con otras figuras en el tauler
-    for (int i = 0; i < figura.getMida(); ++i)
-        for (int j = 0; j < figura[0].size(); ++j)
-            if (figura[i][j] != 0 && tauler[fila + i][columna + j] != 0)
-                esValid = false;
+	return esValid;
+}
 
-    return true;
+bool Tauler::esMovimentValid(const Figura& figura,const Posicio& pos) const
+{
+	bool esValid = true;
+	int i = 0;
+
+	while (i < figura.getMida() && esValid)
+	{
+		int j = 0;
+		while (j < figura.getMida() && esValid)
+		{
+			if (figura.getFigura(i, j) != NO_COLOR)
+			{
+				Posicio pos_tmp;
+				pos_tmp.vertical = pos.vertical + i;
+				pos_tmp.horitzontal = pos.horitzontal + j;
+
+				esValid = posicioValida(pos_tmp);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return esValid;
 }
 
 
-void Tauler::eliminarFilesCompletades()
+int Tauler::eliminarFilesCompletades()
 {
-    // Buscar las filas completadas
+	int nCompletades = 0;
+
+	// Buscar las filas completadas
 	for (int i = 0; i < MAX_FILA; ++i)
 	{
-	    bool completa = true;
-	    int j = 0;
-	
-	    while (j < MAX_COL && completa)
-	    {
-	        if (m_tauler[i][j] == 0) {
-	            completa = false;
-	        }
-	    }
-	    
-	    if (completa)
-	    {
-	        for (int fila = i; fila < MAX_FILA; ++fila )
-	            for (int k = 0; k < MAX_COL; ++k)
-	                m_tauler[fila][k] = m_tauler[fila - 1][k];
-	
-	        for (int k = 0; k < MAX_COL; ++k)
-	            m_tauler[0][k] = COLOR_NEGRE;
-	    }
-	
+		bool completa = true;
+		int j = 0;
+
+		while (j < MAX_COL && completa)
+		{
+			if (m_tauler[i][j] == 0) {
+				completa = false;
+			}
+			j++;
+		}
+
+		if (completa)
+		{
+			nCompletades++;
+			for (int fila = i; fila > 0; --fila)
+				for (int k = 0; k < MAX_COL; ++k)
+					m_tauler[fila][k] = m_tauler[fila - 1][k];
+
+			for (int k = 0; k < MAX_COL; ++k)
+				m_tauler[0][k] = COLOR_NEGRE;
+		}
+
 	}
+	return nCompletades;
 }
 
-void Tauler::collocarFigura()
+void Tauler::eliminarFigura(Figura figura, const Posicio& pos)
 {
+	for (int i = 0; i < figura.getMida(); i++)
+		for (int j = 0; j < figura.getMida(); j++)
+			if (figura.getFigura(i, j) != NO_COLOR)
+			{
+				m_tauler[pos.vertical + i][pos.horitzontal + j] = COLOR_NEGRE;
+			}
+}
 
+void Tauler::collocarFigura( Figura figura,const Posicio& pos) // no comprovo si es valid
+{
+	for (int i = 0; i < figura.getMida(); i++)
+		for (int j = 0; j < figura.getMida(); j++)
+			if (figura.getFigura(i, j) != NO_COLOR)
+			{
+				m_tauler[pos.vertical + i][pos.horitzontal + j] = figura.getColor();
+			}
+	figura.setPosicio(pos);
 }
