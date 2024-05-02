c8bbc65364c2874f07210b33278983f29fa76c9d
NIU1709419 <149013299+NIU1709419@users.noreply.github.com>
2024-05-02 21:54:16 +0200

    Update Joc.h

--- Joc.h
+++ Joc.h
@@ -2,13 +2,15 @@
 #define JOC_H
 #include "Tauler.h"
 #include "Figura.h"
+#include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 class Joc
 {
 public:
-	Joc() {}
+	Joc() { m_posicio.vertical = 0; m_posicio.horitzontal = 0; m_figuraCollocada = false; }
 	void inicialitza(const string& nomFitxer);
 	bool giraFigura(DireccioGir direccio);
 	bool mouFigura(int dirX);
@@ -16,6 +18,10 @@ public:
 	void escriuTauler(const string& nomFitxer);
 
 private:
+	Tauler m_tauler;
+	Figura m_figura;
+	Posicio m_posicio;
+	bool m_figuraCollocada;
 };
 
 #endif
